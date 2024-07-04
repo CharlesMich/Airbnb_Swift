f76d87492fc39541f556d7236cbd50d2f5525aa1
CharlesMich <charleedongo@hotmail.com>
2024-07-04 13:06:12 -0500

    Initial Commit

---
+++ airbnb/airbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnbApp.swift
+//  airbnb
+//
+//  Created by Charles Michael on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct airbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
