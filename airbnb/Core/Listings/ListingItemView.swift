e8ea912f0d72eae9f656177db01b01fa35fde558
CharlesMich <charleedongo@hotmail.com>
2024-07-05 16:59:02 -0500

    july 5

---
+++ airbnb/Core/Listings/ListingItemView.swift
@@ -0,0 +1,18 @@
+Item//
+//  ListingView.swift
+//  airbnb
+//
+//  Created by Charles Michael on 7/5/24.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
