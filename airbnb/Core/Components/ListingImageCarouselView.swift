052ad5fddbac851ecba0b9c5e145f79861b7e53e
CharlesMich <charleedongo@hotmail.com>
2024-07-25 17:07:57 -0500

    jul 25

---
+++ airbnb/Core/Components/ListingImageCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingImageCarouselView.swift
+//  airbnb
+//
+//  Created by Charles Michael on 7/25/24.
+//
+
+import SwiftUI
+
+struct ListingImageCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingImageCarouselView()
+}
