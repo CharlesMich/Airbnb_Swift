0253bdb08b9847d8ae7ed762b965f166819822b9
CharlesMich <charleedongo@hotmail.com>
2024-08-01 11:39:47 -0500

    aug 1

--- airbnb/Core/Listings/ListingDetailView.swift
+++ airbnb/Core/Listings/ListingDetailView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ListingDetailView: View {
     var images = [
@@ -14,10 +15,27 @@ struct ListingDetailView: View {
     "listine-3",
     "listing-4",
     ]
+    
+    @Environment(\.dismiss) var dismiss
     var body: some View {
         ScrollView {
-            ListingImageCarouselView()
-                .frame(height:320)
+            ZStack (alignment: .topLeading) {
+                ListingImageCarouselView()
+                    .frame(height:320)
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .foregroundStyle(.black)
+                        .background{
+                            Circle()
+                                .fill(.white)
+                                .frame(width:32, height:32)
+                        }
+                        .padding(32)
+                }
+               
+            }
             
             VStack(alignment: .leading, spacing: 0){
                 Text("Miami VIlla")
@@ -90,7 +108,7 @@ struct ListingDetailView: View {
             .padding()
             Divider()
             
-            VStack(alignment: .leading){
+            VStack(alignment: .leading, spacing:16){
                 Text("Where you will sleep")
                     .font(.headline)
                 ScrollView(.horizontal, showsIndicators: false){
@@ -102,10 +120,88 @@ struct ListingDetailView: View {
                                 Image(systemName: "bed.double")
                                 Text("Bedroom \(bedroom)")
                             }
+                            .frame(width:132, height:100)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(lineWidth: 1)
+                                    .foregroundStyle(.gray)
+                            }
                         }
                     }
                 }
+                .scrollTargetBehavior(.paging)
             }
+            .padding()
+            
+            Divider()
+            
+//            listing amenities
+            
+            VStack(alignment: .leading, spacing:16){
+            Text("What this place offers")
+                    .font(.headline)
+                ForEach(0 ..< 5) {
+                    feature in
+                    HStack {
+                        Image(systemName:"wifi")
+                            .frame(width: 32)
+                        
+                        Text("wifi")
+                            .font(.footnote)
+                        Spacer()
+                    }
+                }
+            }
+            .padding()
+            
+            Divider()
+            
+            VStack(alignment: .leading, spacing:16){
+                Text("where you will be")
+                
+                Map()
+                    .frame(height:200)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+            .padding()
+        }
+        .ignoresSafeArea()
+        .padding(.bottom, 64)
+        .overlay(alignment:.bottom){
+            VStack {
+                Divider()
+                    .padding(.bottom)
+                
+                HStack {
+                    VStack (alignment:.leading){
+                        Text("$500")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                        Text("Total before taxes")
+                            .font(.footnote)
+                        Text("Oct15-20")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                            .underline()
+                        
+                    }
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Text("Reserve")
+                            .foregroundStyle(.white)
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 140, height: 40)
+                            .background(.pink)
+                            .clipShape(RoundedRectangle(cornerRadius:8))
+                        
+                    }
+                }
+                .padding(.horizontal,32)
+            }
+            .background(.white)
         }
     }
 }
